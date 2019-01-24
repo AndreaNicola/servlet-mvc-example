99312efe3af77f6a3a1539b25016d3e86516863c
Andrea Nicola <cico,85@gmail.com>
2019-01-24 20:54:25 +0100

    Update BookInsertServlet.java
    Begin to work on bookInsert controller

--- src/controllers/insert/BookInsertServlet.java
+++ src/controllers/insert/BookInsertServlet.java
@@ -1,5 +1,8 @@
 package controllers.insert;
 
+import dao.FactoryDao;
+import dao.IAutoreDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,11 +13,19 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/bookInsert", name = "BookInsertServlet")
 public class BookInsertServlet extends HttpServlet {
 
+    private static final IAutoreDao I_AUTORE_DAO = FactoryDao.getiAutoreDao();
+    private static final I
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Object genres = request.getParameter("genres");
+
+
         request.getRequestDispatcher("/WEB-INF/jsp/form/bookForm.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("action", "bookInsert");
         request.getRequestDispatcher("/WEB-INF/jsp/form/bookForm.jsp").forward(request, response);
     }
 }
